ef024c84a7fd9580c98ec785ce7033778a19c664
getoconas <getoconas@gmail.com>
2024-08-18 22:19:14 -0300

    reestructuracion de las clases alojamiento y turista

--- src/controller/alojamiento.py
+++ src/controller/alojamiento.py
@@ -1,4 +1,17 @@
 # Definción de la clase alojamiento
+from dataclasses import dataclass
+
+@dataclass
+class Alojamiento:
+  nombre: str
+  categoria: str
+  precio: int
+  direccion: str
+  telefono: int
+  imagen: str
+  tipo: str
+
+"""
 class Alojamiento():
   def __init__(self, nombre, categoria, precio, direccion, telefono, imagen, tipo) -> None:
     self.nombre = nombre
@@ -8,3 +21,4 @@ class Alojamiento():
     self.telefono = telefono
     self.imagen = imagen
     self.tipo = tipo
+"""
--- src/controller/turista.py
+++ src/controller/turista.py
@@ -1,4 +1,37 @@
 # Definición de la clase turista
+from dataclasses import dataclass
+
+@dataclass
+class Turista:
+  persona: str
+  presupuesto: str
+  estadia: str
+
+  @classmethod
+  def from_selections(cls, persona: int, presupuesto: int, estadia: int):
+    persona_map = {
+      0: 'PER1',
+      1: 'PER2',
+      2: 'PER3'
+    }
+    presupuesto_map = {
+      0: 'PRB',
+      1: 'PRM',
+      2: 'PRA'
+    }
+    estadia_map = {
+      0: 'EST1',
+      1: 'EST2',
+      2: 'EST3'
+    }
+
+    return cls (
+      persona = persona_map.get(persona, 'PER3'),
+      presupuesto = presupuesto_map.get(presupuesto, 'PRA'),
+      estadia = estadia_map.get(estadia, 'EST3')
+    )
+
+"""
 class Turista():
   def __init__(self, persona, presupuesto, estadia) -> None:
     
@@ -22,3 +55,4 @@ class Turista():
       self.estadia = 'EST2'
     else:
       self.estadia = 'EST3'
+"""
